84e9df14a5b669441c370c492fe7c45e588738fc
gonzalo-b4 <williamcasa973@gmail.com>
2025-02-10 14:11:29 -0300

    Delete caca - 100%/marketplace directory

--- caca - 100%/marketplace/backend/middlewares/adminAuth.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (req, res, next) {
-    console.log('User Role:', req.user.role); // Agregar log para verificar el rol del usuario
-    if (req.user.role !== 'admin') {
-        return res.status(403).json({ msg: 'Access denied' });
-    }
-    next();
-};
